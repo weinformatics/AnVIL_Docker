9a3bf9bb307ede2fac7fdb7376259b1e3e50261b
Nitesh Turaga <nitesh.turaga@gmail.com>
2019-01-17 10:21:26 -0500

    Update docker containers for Rstudio bioc3.9
    - Update install.R script to use BiocManager::install
    - Remove /tmp/install.R from container as cleanup

--- rstudio/bioc_3.9/install.R
+++ rstudio/bioc_3.9/install.R
@@ -2,25 +2,17 @@ if ("BiocManager" %in% rownames(installed.packages()))
 	remove.packages("BiocManager")
 
 install.packages("BiocManager", repos="https://cran.rstudio.com")
-install.packages("devtools", repos="https://cran.rstudio.com")
 
-library(BiocManager)
+if(BiocManager::version() != "3.9")
+    BiocManager::install(version="3.9", update=TRUE, ask=FALSE)
 
-if(BiocManager::version() != "3.8"){
-    BiocManager::install(version="3.8",
-                         update=TRUE, ask=FALSE)
-}
+BiocManager::install("devtools")
 
 builtins <- c("Matrix", "KernSmooth", "mgcv")
-
-for (builtin in builtins)
-    if (!suppressWarnings(require(builtin, character.only=TRUE)))
-        suppressWarnings(BiocManager::install(builtin,
-                                              version="3.8",
-                                              update=TRUE, ask=FALSE))
-
-
-install.packages(c("bigrquery", "googleCloudStorageR"),
-                 repos="http://cran.mtu.edu")
-
-devtools::install_github("DataBiosphere/Ronaldo")
+pkgs <- c(
+    builtins[!builtins %in% rownames(installed.packages())],
+    "bigrquery", "googleCloudStorageR",
+    "DataBiosphere/Ronaldo"
+    ## Add additional comma-separated packages here
+)
+BiocManager::install(pkgs, update = TRUE, ask = FALSE)
