ec4be8f6850f01b8cbd30a4d0c086891f22a25c2
Nitesh Turaga <nitesh.turaga@gmail.com>
2019-01-20 11:43:23 -0500

    Minor changes in run script,
    The scripts localize.py and delocalize.py are useful for buckets. This
    is something I need to keep an eye on once the base-R docker image and
    bucket are launched.

--- jupyter/bioc_3.9/run-jupyter-local.sh
+++ jupyter/bioc_3.9/run-jupyter-local.sh
@@ -4,7 +4,7 @@
 #   docker build jupyter-docker/ -t broadinstitute/leonardo-notebooks:local
 set -e
 
-DOCKER_IMG=broadinstitute/leonardo-notebooks:local
+DOCKER_IMG=bioconductor/anvil-jupyter-bioc3.9
 CONTAINER=jupyter-server
 
 start () {
@@ -17,9 +17,9 @@ start () {
 
     echo "Starting Jupyter server container..."
     docker create -it --rm --name ${CONTAINER} -p 8000:8000 -e GOOGLE_PROJECT=project -e CLUSTER_NAME=cluster "${DOCKER_IMG}"
-    docker cp jupyter-docker/jupyter_notebook_config.py ${CONTAINER}:/etc/jupyter/jupyter_notebook_config.py
-    docker cp jupyter-docker/jupyter_localize_extension.py ${CONTAINER}:/etc/jupyter/custom/jupyter_localize_extension.py
-    docker cp jupyter-docker/jupyter_delocalize.py ${CONTAINER}:/etc/jupyter/custom/jupyter_delocalize.py
+#    docker cp jupyter-docker/jupyter_notebook_config.py ${CONTAINER}:/etc/jupyter/jupyter_notebook_config.py
+#    docker cp jupyter-docker/jupyter_localize_extension.py ${CONTAINER}:/etc/jupyter/custom/jupyter_localize_extension.py
+#    docker cp jupyter-docker/jupyter_delocalize.py ${CONTAINER}:/etc/jupyter/custom/jupyter_delocalize.py
     # To debug startup failures, add -a here to attach.
     docker start -a ${CONTAINER}
 
